73d7b22d7dcb496485c7b567b59be7e396bf9ba9
oliverweissl <o.weissl@student.vu.nl>
2021-01-10 11:38:23 +0100

    0.2.7

--- intelligent_agent.py
+++ intelligent_agent.py
@@ -1,13 +1,15 @@
 import martix_gen
 import matplotlib
 import matplotlib.pyplot as pyplot
-#from api import State, util
+from api import State, util
 import random
 
+"""
 #test matrix
 playedCards = [1,12,9,6,2]
 playerHand = [3,5,15,16,7]
-one = martix_gen.Cost_Generator(True, 19,playedCards,playerHand)
+opponentTrick = 13
+one = martix_gen.Cost_Generator(True, 19,playedCards,playerHand,opponentTrick)
 one.populator()
 print(one.matrix)
 
@@ -17,9 +19,28 @@ pyplot.show()
 class Bot:
 
     def __init__(self):
-
+        pass
     def get_move(self, state):
-        player = state.whose_turn() #player on move
         moves = state.moves() #legal moves
+        chosen_move = random.choice(moves)  # initialise chosen move as random by default
 
-"""
+        trump_suite = state.get_trump_suit()
+
+        playedCards = [] #all previous played cards
+        initial_turn = True #if you have the initial turn or not
+
+        playerHand = []#used for the cost matrix
+        for move in moves:
+            if move[0] is not None: playerHand.append(move[0])
+            else: chosen_move = move #if trump exchange is available, play it
+
+        if state.get_opponents_played_card() is not None:
+            initial_turn = False
+            opponentTrick = state.get_opponents_played_card()
+        else:
+            initial_turn = False
+            opponentTrick = None
+
+
+
+        return chosen_move
--- martix_gen.py
+++ martix_gen.py
@@ -18,16 +18,11 @@ TRUMP_MULTIPLIER = 4
 
 #defining card value based on index
 def get_value(index):
-    if index%5 == 0:
-        return 11
-    elif index%5 == 1:
-        return 10
-    elif index%5 == 2:
-        return 5
-    elif index%5 == 3:
-        return 4
-    else:
-        return 3
+    if index%5 == 0: return 11
+    elif index%5 == 1: return 10
+    elif index%5 == 2: return 5
+    elif index%5 == 3: return 4
+    else: return 3
 
 def get_suite(index):
     return int(index/5)
@@ -44,13 +39,15 @@ class Matrix:
 #use the generator with the index of the trump card
 #played cards should be array of integer
 class Cost_Generator:
-    def __init__(self,firstMove, trumpCard, playedCards,playerHand):
-        self.firstMove = firstMove
-        self.trumpCard = trumpCard
-        self.playedCards = playedCards
-        self.playerHand = playerHand
+    def __init__(self,firstMove, trumpCard, playedCards,playerHand, opponentTrick):
+        self.firstMove = firstMove #if the player has the first move
+        self.trumpCard = trumpCard #the trump card
+        self.playedCards = playedCards #all previous played cards
+        self.playerHand = playerHand #the current cards of the player
+        self.opponentTrick = opponentTrick #if self.firstMove == False: the card played by opponent
 
-        self.matrix = Matrix(CARD_AMOUNT).matrix #pre-populate matrix
+        self.matrix = Matrix(CARD_AMOUNT).matrix #pre-populated Q-matrix
+
 
 
     #checks if a card is present in an array
@@ -64,39 +61,45 @@ class Cost_Generator:
 
     def populator(self):
 
-        #generate all card pairs
-        #format: [card_player_1, card_player_2]
-        combinations = []
-        for i in range(CARD_AMOUNT):
-            for j in self.playerHand:
-                combinations.append([i, j])
-        #print(combinations)
-
         trump = int(self.trumpCard / 5)  # get the suite of the trump card
+        combinations = []
 
+        # generate all card pairs
+        # format: [card_player_1, card_player_2]
         if self.firstMove == True:
-            #calcualte scores
-            #format cards
-            #populate the matrix
-            for pair in combinations:
+            for i in range(CARD_AMOUNT):
+                for j in self.playerHand:
+                    combinations.append([i, j])
+            # print(combinations)
+        else:
+            for j in self.playerHand:
+                combinations.append([self.opponentTrick, j])
 
-                suite_player = get_suite(pair[0]) # get suite of the players card
-                suite_opponent = get_suite(pair[1])  # get suite of the opponent
 
-                value_player = get_value(pair[0])  # value of the players card
-                value_opponent = get_value(pair[1])  # value of the opponents card
+        # calcualte scores
+        # format cards
+        # populate the matrix
+        for pair in combinations:
 
-                # adjust card value if trump
-                if suite_player == trump:
-                    value_player *= TRUMP_MULTIPLIER
-                if suite_opponent == trump:
-                    value_opponent *= TRUMP_MULTIPLIER
+            suite_player = get_suite(pair[0])  # get suite of the players card
+            suite_opponent = get_suite(pair[1])  # get suite of the opponent
 
-                #populate matrix with score
-                if pair[0] == pair[1]:
-                    self.matrix[pair[0],pair[1]] = -1 #rule out illegal moves
+            value_player = get_value(pair[0])  # value of the players card
+            value_opponent = get_value(pair[1])  # value of the opponents card
 
-                elif self.is_present(self.playedCards,pair[0]) == True or self.is_present(self.playedCards,pair[1]) == True:
-                    self.matrix[pair[0], pair[1]] = -1 #blackout moves with unavaliable cards
-                else:
-                    self.matrix[pair[0], pair[1]] = value_player/ value_opponent #evaluate moves (winning moves have higher scores)
+            # adjust card value if trump
+            if suite_player == trump:
+                value_player *= TRUMP_MULTIPLIER
+            if suite_opponent == trump:
+                value_opponent *= TRUMP_MULTIPLIER
+
+            # populate matrix with score
+            if pair[0] == pair[1]:
+                self.matrix[pair[0], pair[1]] = -1  # rule out illegal moves
+
+            elif self.is_present(self.playedCards, pair[0]) == True or self.is_present(self.playedCards,
+                                                                                       pair[1]) == True:
+                self.matrix[pair[0], pair[1]] = -1  # blackout moves with unavaliable cards
+            else:
+                self.matrix[pair[0], pair[
+                    1]] = value_player / value_opponent  # evaluate moves (winning moves have higher scores)
